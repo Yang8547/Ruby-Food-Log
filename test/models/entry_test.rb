3f0938325a955c4fb2e4bb9ac0f59c3c9f6131a2
yang <anyang47@hotmail.com>
2020-10-16 13:39:46 -0400

    modify some controller test and model test

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,8 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'is valid with valid attributes' do
+    entry = Entry.new(meal_type: 'breakfast', carbohydrates: 35, proteins: 20, fats: 30, calories: 400)
+    assert entry.save
+  end
 end
